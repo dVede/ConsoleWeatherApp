3bf8d3acf3126d57224068451712b629bf2c9218
Danil Medvedev <dvede123@yandex.ru>
2021-11-28 18:59:00 +0300

    Add error messages in next cases -No internet connection -City not found -Incorrect measurement unit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,14 @@
 import org.json.JSONObject
 import java.io.BufferedReader
+import java.io.FileNotFoundException
 import java.io.InputStreamReader
 import java.net.URL
+import java.net.UnknownHostException
 
 import kotlin.math.floor
 
 private val units = listOf("imperial","metric","standard")
+private const val API_KEY = "0f21bd02afdc24c032045afd167ff588"
 
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
@@ -17,17 +20,26 @@ fun main(args: Array<String>) {
         return
     }
     var unit = "standard"
-    if (args.size == 2 && units.contains(args[1].lowercase())) {
-        unit = args[1]
+    if (args.size == 2) {
+        if (units.contains(args[1].lowercase()))
+            unit = args[1]
+        else
+            println("Единица измерения ${args[1]} не найдена, использована единица измерения $unit")
     }
     val urlAddress = "https://api.openweathermap.org/data/2.5/weather?q=${args[0]}" +
-            "&appid=0f21bd02afdc24c032045afd167ff588&units=${unit}&lang=ru"
-    val output = getWeatherInfo(urlAddress)
-    if (output.isNotBlank()) {
+            "&appid=$API_KEY&units=$unit&lang=ru"
+    try {
+        val output = getWeatherInfo(urlAddress)
         val obj = JSONObject(output)
         println(generalInfo(obj))
         println(temperatureInfo(obj))
         println(windInfo(obj))
+    }
+    catch (e: FileNotFoundException) {
+        println("Такой город не найден")
+    }
+    catch (e: UnknownHostException) {
+        println("Нет подключения к интернету")
     }
 }
 
@@ -63,13 +75,9 @@ private fun getWindDirection(degrees: Double): String {
 }
 
 private fun getWeatherInfo(urlAddress: String): String {
-    var result = String()
-    try {
-        val urlConnection = URL(urlAddress).openConnection()
-        val stream = InputStreamReader(urlConnection.getInputStream())
-        result = stream.buffered().use(BufferedReader::readText)
-    } catch (e: Exception) {
-        println("Такой город не найден!")
-    }
+    val result: String
+    val urlConnection = URL(urlAddress).openConnection()
+    val stream = InputStreamReader(urlConnection.getInputStream())
+    result = stream.buffered().use(BufferedReader::readText)
     return result
 }
